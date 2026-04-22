e1c1f8bbfcc8c10af036c3be91ad341586567105
Amer-Shareef <141691359+Amer-Shareef@users.noreply.github.com>
2026-04-22 07:55:34 +0530

    Changed Css in the Hero

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-black">
-    {/* Hero Section*/}
-    <section className="min-h-full w-full relative bg-linear-to-b from-black from-15% via-gray-400/60 via-50% to-black to-85%">
-      <div className="flex flex-col flex-1 items-center justify-center text-center gap-10">
-        <h1 className="font-extrabold text-[242px] mt-10">LUXURY CAR</h1>
-        <Image
-          src="/hero-image.png"
-          width={4096}
-          height={2048}
-          alt="Picture of the author"
-          className="z-2 "
-        />
-      </div>
-    </section>
+      {/* Hero Section*/}
+      <section className="min-h-screen flex items-center justify-center relative overflow-hidden bg-linear-to-b from-black from-15% via-gray-400/60 via-50% to-black to-85%">
+        <div className="w-full flex flex-col flex-1 items-center justify-center text-center gap-10 relative">
+          <h1 className="absolute -top-[20%] font-extrabold text-[242px] mt-10">
+            LUXURY CAR
+          </h1>
+          <div className="z-2">
+            <Image
+              src="/hero-image.png"
+              width={4096}
+              height={2048}
+              alt="Car Image"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="min-h-screen flex items-center justify-center relative overflow-hidden">
+        <div>
+          <h2>Car Inventory</h2>
+        </div>
+      </section>
     </main>
   );
 }
