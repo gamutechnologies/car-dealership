3a0860c96667d59327214243d9dd3b6ad2fb900d
Amer-Shareef <141691359+Amer-Shareef@users.noreply.github.com>
2026-05-08 09:42:06 +0530

    Overview

--- components/overview/image-gallery.tsx
+++ components/overview/image-gallery.tsx
@@ -1,8 +1,116 @@
+import Image from "next/image";
+import { ArrowUpRightIcon } from "lucide-react";
+
 export default function ImageGallerySection() {
   return (
-    <section className="min-h-screen py-32 flex flex-col items-center relative overflow-hidden bg-black">
-      <div className="relative items-center justify-center text-center">
-        Image Gallery Section
+    <section className="min-h-screen py-20 flex flex-col items-center justify-end relative overflow-hidden bg-black">
+      <div className="absolute top-0 left-0 w-full h-2/3">
+        <Image
+          src="/images/car-black.webp"
+          alt="Ferrari 296 GTB"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="min-w-7xl flex flex-col relative z-10">
+        <div className="flex flex-row justify-between gap-4 mb-8">
+          <div className="relative w-52 h-28 rounded-2xl overflow-hidden border border-zinc-700">
+            <Image
+              src="/images/car-blue.webp"
+              alt="Blue Car"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-52 h-28 rounded-2xl overflow-hidden border border-zinc-700">
+            <Image
+              src="/images/car-red.webp"
+              alt="Red Car"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-52 h-28 rounded-2xl overflow-hidden border-2 border-yellow-500 shadow-lg shadow-yellow-500/20">
+            <Image
+              src="/images/car-black.webp"
+              alt="Black Car"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-52 h-28 rounded-2xl overflow-hidden border border-zinc-700">
+            <Image
+              src="/images/car-purple.webp"
+              alt="Purple Car"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-52 h-28 rounded-2xl overflow-hidden border border-zinc-700">
+            <Image
+              src="/images/car-white.webp"
+              alt="White Car"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-52 h-28 rounded-2xl overflow-hidden border border-zinc-700">
+            <Image
+              src="/images/car-orange.webp"
+              alt="Orange Car"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+        <div className="w-full rounded-2xl bg-zinc-900/90 backdrop-blur-md border border-zinc-800 px-6 py-4 flex flex-row justify-between items-center">
+          <div className="flex flex-col">
+            <h2 className="text-4xl font-heading text-white mb-4">
+              Ferrari 296 GTB 2024
+            </h2>
+            <div className="flex flex-row items-center gap-4">
+              <div className="flex flex-row">
+                <div className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-black z-30">
+                  <Image
+                    src="/images/joseph-vijay.webp"
+                    alt="Joseph Vijay"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-black -ml-3 z-20">
+                  <Image
+                    src="/images/ajith-kumar.webp"
+                    alt="Ajith Kumar"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-black -ml-3 z-10">
+                  <Image
+                    src="/images/osama-bin-laden.webp"
+                    alt="Osama bin Laden"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              </div>
+              <div>
+                <p className="text-lg font-medium text-white">20+</p>
+                <p className="text-sm text-zinc-400">
+                  Trusted to use by millions users over 140 countries
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-end">
+            <h3 className="text-4xl font-heading text-white mb-4">$620,000</h3>
+            <button className="bg-yellow-500 text-black text-base font-medium px-8 py-2 rounded-full flex flex-row items-center gap-2">
+              Book Now
+              <ArrowUpRightIcon size={20} />
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
--- components/overview/specifications.tsx
+++ components/overview/specifications.tsx
@@ -1,9 +1,58 @@
 export default function SpecificationsSection() {
   return (
     <section className="min-h-screen flex items-center justify-center relative overflow-hidden bg-black bg-[url(/images/specification-bg.webp)] bg-cover bg-center">
-      <div className="max-w-7xl flex flex-col flex-1 grid-cols-2 gap-10 relative">
-        <div className="relative w-1/2">
-          <div></div>
+      <div className="min-w-7xl flex flex-col justify-between h-screen py-8">
+        <div className="w-1/3 rounded-xl bg-white/10 backdrop-blur-sm border border-white/20 p-8">
+          <h2 className="text-4xl font-semibold text-white mb-4">
+            Key Specifications
+          </h2>
+          <div className="grid grid-cols-2 gap-y-6 gap-x-5 text-white">
+            <div>
+              <p className="text-lg mb-3">↗ Engine</p>
+              <p className="text-xs text-zinc-300">
+                3.0L Twin-Turbo V6 + Electric Motor
+              </p>
+            </div>
+            <div>
+              <p className="text-lg mb-3">↗ Torque</p>
+              <p className="text-xs text-zinc-300">740 Nm</p>
+            </div>
+            <div>
+              <p className="text-lg mb-3">↗ Power</p>
+              <p className="text-xs text-zinc-300">830 hp (combined)</p>
+            </div>
+            <div>
+              <p className="text-lg mb-3">↗ Transmission</p>
+              <p className="text-xs text-zinc-300">8-Speed DCT</p>
+            </div>
+            <div>
+              <p className="text-lg mb-3">↗ 0-100 km/h</p>
+              <p className="text-xs text-zinc-300">~2.9 sec</p>
+            </div>
+            <div>
+              <p className="text-lg mb-3">↗ Top Speed</p>
+              <p className="text-xs text-zinc-300">~330 km/h</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-6xl rounded-xl bg-white/10 backdrop-blur-sm border border-white/20 pt-10 px-8 ">
+          <div className="flex flex-row items-start">
+            <div className="w-25">
+              <p className="text-base text-white">Overview</p>
+            </div>
+
+            <div className="flex-1">
+              <p className="text-4xl leading-tight text-white font-light">
+                The 2024 Ferrari 296 GTB is a hybrid V6 supercar delivering 830
+                hp of breathtaking performance. With its sharp aerodynamics,
+                electric boost,{" "}
+                <span className="text-zinc-400">
+                  and iconic Ferrari handling, it blends pure speed with modern
+                  efficiency in a compact, beautifully engineered package.
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
